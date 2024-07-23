e21f91c28c6976cd1f714c4a694ef48f73a78cc4
MUHAMMAD AYOUB <mayoub7114@gmail.com>
2024-07-23 11:56:06 +0500

    Updated ViewTotalIncome component to include correct total calculations

--- src/features/customer/Navbar.js
+++ src/features/customer/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='mb-8 px-4'>
-      <div className="flex justify-center items-center h-16">
-        <h1 className="text-base sm:text-lg md:text-2xl lg:text-2xl xl:text-3xl font-extrabold mt-10 text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 whitespace-nowrap">
+    <div className='mb-4 mt-4 px-4'>
+      <div className="flex justify-center items-center h-24">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 whitespace-nowrap">
           فہد موٹرز سٹھاری ٹاؤن جتوئ
         </h1>
       </div>
